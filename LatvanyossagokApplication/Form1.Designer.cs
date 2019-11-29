85796c565b735b1499a71d79d3a776e3e949f20f
TorokNiki <niki.torok@gmail.com>
2019-11-29 12:43:42 +0100

    második feladat és kicsi harmadik

--- LatvanyossagokApplication/Form1.Designer.cs
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -28,13 +28,329 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBoxVaros = new System.Windows.Forms.GroupBox();
+            this.buttonVarosFeltolt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownVarosLakossag = new System.Windows.Forms.NumericUpDown();
+            this.textBoxVarosNev = new System.Windows.Forms.TextBox();
+            this.groupBoxLatvanyossag = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBoxVarosNevek = new System.Windows.Forms.ListBox();
+            this.buttonLFeltolt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownLAr = new System.Windows.Forms.NumericUpDown();
+            this.textBoxLNev = new System.Windows.Forms.TextBox();
+            this.groupBoxFunkciok = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonLatvanyossagModosit = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBoxLatvanyossagok = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDownVarosLakossagModosit = new System.Windows.Forms.NumericUpDown();
+            this.textBoxVarosNevModosit = new System.Windows.Forms.TextBox();
+            this.buttonTorles = new System.Windows.Forms.Button();
+            this.listBoxVarosok = new System.Windows.Forms.ListBox();
+            this.groupBoxVaros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVarosLakossag)).BeginInit();
+            this.groupBoxLatvanyossag.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLAr)).BeginInit();
+            this.groupBoxFunkciok.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVarosLakossagModosit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxVaros
+            // 
+            this.groupBoxVaros.Controls.Add(this.buttonVarosFeltolt);
+            this.groupBoxVaros.Controls.Add(this.label2);
+            this.groupBoxVaros.Controls.Add(this.label1);
+            this.groupBoxVaros.Controls.Add(this.numericUpDownVarosLakossag);
+            this.groupBoxVaros.Controls.Add(this.textBoxVarosNev);
+            resources.ApplyResources(this.groupBoxVaros, "groupBoxVaros");
+            this.groupBoxVaros.Name = "groupBoxVaros";
+            this.groupBoxVaros.TabStop = false;
+            // 
+            // buttonVarosFeltolt
+            // 
+            this.buttonVarosFeltolt.BackColor = System.Drawing.Color.LightGreen;
+            resources.ApplyResources(this.buttonVarosFeltolt, "buttonVarosFeltolt");
+            this.buttonVarosFeltolt.Name = "buttonVarosFeltolt";
+            this.buttonVarosFeltolt.UseVisualStyleBackColor = false;
+            this.buttonVarosFeltolt.Click += new System.EventHandler(this.buttonVarosFeltolt_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // numericUpDownVarosLakossag
+            // 
+            resources.ApplyResources(this.numericUpDownVarosLakossag, "numericUpDownVarosLakossag");
+            this.numericUpDownVarosLakossag.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownVarosLakossag.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownVarosLakossag.Name = "numericUpDownVarosLakossag";
+            this.numericUpDownVarosLakossag.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBoxVarosNev
+            // 
+            resources.ApplyResources(this.textBoxVarosNev, "textBoxVarosNev");
+            this.textBoxVarosNev.Name = "textBoxVarosNev";
+            // 
+            // groupBoxLatvanyossag
+            // 
+            this.groupBoxLatvanyossag.Controls.Add(this.label5);
+            this.groupBoxLatvanyossag.Controls.Add(this.listBoxVarosNevek);
+            this.groupBoxLatvanyossag.Controls.Add(this.buttonLFeltolt);
+            this.groupBoxLatvanyossag.Controls.Add(this.label3);
+            this.groupBoxLatvanyossag.Controls.Add(this.label4);
+            this.groupBoxLatvanyossag.Controls.Add(this.numericUpDownLAr);
+            this.groupBoxLatvanyossag.Controls.Add(this.textBoxLNev);
+            resources.ApplyResources(this.groupBoxLatvanyossag, "groupBoxLatvanyossag");
+            this.groupBoxLatvanyossag.Name = "groupBoxLatvanyossag";
+            this.groupBoxLatvanyossag.TabStop = false;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // listBoxVarosNevek
+            // 
+            this.listBoxVarosNevek.FormattingEnabled = true;
+            resources.ApplyResources(this.listBoxVarosNevek, "listBoxVarosNevek");
+            this.listBoxVarosNevek.Name = "listBoxVarosNevek";
+            // 
+            // buttonLFeltolt
+            // 
+            this.buttonLFeltolt.BackColor = System.Drawing.Color.LightGreen;
+            resources.ApplyResources(this.buttonLFeltolt, "buttonLFeltolt");
+            this.buttonLFeltolt.Name = "buttonLFeltolt";
+            this.buttonLFeltolt.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // numericUpDownLAr
+            // 
+            resources.ApplyResources(this.numericUpDownLAr, "numericUpDownLAr");
+            this.numericUpDownLAr.Name = "numericUpDownLAr";
+            // 
+            // textBoxLNev
+            // 
+            resources.ApplyResources(this.textBoxLNev, "textBoxLNev");
+            this.textBoxLNev.Name = "textBoxLNev";
+            // 
+            // groupBoxFunkciok
+            // 
+            this.groupBoxFunkciok.Controls.Add(this.label8);
+            this.groupBoxFunkciok.Controls.Add(this.listBox1);
+            this.groupBoxFunkciok.Controls.Add(this.buttonLatvanyossagModosit);
+            this.groupBoxFunkciok.Controls.Add(this.label9);
+            this.groupBoxFunkciok.Controls.Add(this.label10);
+            this.groupBoxFunkciok.Controls.Add(this.numericUpDown1);
+            this.groupBoxFunkciok.Controls.Add(this.textBox1);
+            this.groupBoxFunkciok.Controls.Add(this.listBoxLatvanyossagok);
+            this.groupBoxFunkciok.Controls.Add(this.button1);
+            this.groupBoxFunkciok.Controls.Add(this.label6);
+            this.groupBoxFunkciok.Controls.Add(this.label7);
+            this.groupBoxFunkciok.Controls.Add(this.numericUpDownVarosLakossagModosit);
+            this.groupBoxFunkciok.Controls.Add(this.textBoxVarosNevModosit);
+            this.groupBoxFunkciok.Controls.Add(this.buttonTorles);
+            this.groupBoxFunkciok.Controls.Add(this.listBoxVarosok);
+            resources.ApplyResources(this.groupBoxFunkciok, "groupBoxFunkciok");
+            this.groupBoxFunkciok.Name = "groupBoxFunkciok";
+            this.groupBoxFunkciok.TabStop = false;
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Name = "listBox1";
+            // 
+            // buttonLatvanyossagModosit
+            // 
+            this.buttonLatvanyossagModosit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.buttonLatvanyossagModosit, "buttonLatvanyossagModosit");
+            this.buttonLatvanyossagModosit.Name = "buttonLatvanyossagModosit";
+            this.buttonLatvanyossagModosit.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // numericUpDown1
+            // 
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
+            this.numericUpDown1.Name = "numericUpDown1";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // listBoxLatvanyossagok
+            // 
+            this.listBoxLatvanyossagok.FormattingEnabled = true;
+            resources.ApplyResources(this.listBoxLatvanyossagok, "listBoxLatvanyossagok");
+            this.listBoxLatvanyossagok.Name = "listBoxLatvanyossagok";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // numericUpDownVarosLakossagModosit
+            // 
+            resources.ApplyResources(this.numericUpDownVarosLakossagModosit, "numericUpDownVarosLakossagModosit");
+            this.numericUpDownVarosLakossagModosit.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownVarosLakossagModosit.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownVarosLakossagModosit.Name = "numericUpDownVarosLakossagModosit";
+            this.numericUpDownVarosLakossagModosit.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBoxVarosNevModosit
+            // 
+            resources.ApplyResources(this.textBoxVarosNevModosit, "textBoxVarosNevModosit");
+            this.textBoxVarosNevModosit.Name = "textBoxVarosNevModosit";
+            // 
+            // buttonTorles
+            // 
+            this.buttonTorles.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.buttonTorles, "buttonTorles");
+            this.buttonTorles.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonTorles.Name = "buttonTorles";
+            this.buttonTorles.UseVisualStyleBackColor = false;
+            // 
+            // listBoxVarosok
+            // 
+            this.listBoxVarosok.FormattingEnabled = true;
+            resources.ApplyResources(this.listBoxVarosok, "listBoxVarosok");
+            this.listBoxVarosok.Name = "listBoxVarosok";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBoxFunkciok);
+            this.Controls.Add(this.groupBoxLatvanyossag);
+            this.Controls.Add(this.groupBoxVaros);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxVaros.ResumeLayout(false);
+            this.groupBoxVaros.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVarosLakossag)).EndInit();
+            this.groupBoxLatvanyossag.ResumeLayout(false);
+            this.groupBoxLatvanyossag.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLAr)).EndInit();
+            this.groupBoxFunkciok.ResumeLayout(false);
+            this.groupBoxFunkciok.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVarosLakossagModosit)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxVaros;
+        private System.Windows.Forms.Button buttonVarosFeltolt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownVarosLakossag;
+        private System.Windows.Forms.TextBox textBoxVarosNev;
+        private System.Windows.Forms.GroupBox groupBoxLatvanyossag;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBoxVarosNevek;
+        private System.Windows.Forms.Button buttonLFeltolt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownLAr;
+        private System.Windows.Forms.TextBox textBoxLNev;
+        private System.Windows.Forms.GroupBox groupBoxFunkciok;
+        private System.Windows.Forms.ListBox listBoxLatvanyossagok;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDownVarosLakossagModosit;
+        private System.Windows.Forms.TextBox textBoxVarosNevModosit;
+        private System.Windows.Forms.Button buttonTorles;
+        private System.Windows.Forms.ListBox listBoxVarosok;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonLatvanyossagModosit;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -19,10 +19,12 @@ namespace LatvanyossagokApplication
         {
             InitializeComponent();
             conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
-            conn.Open();
-            latvanyosagTablaLetrehozas();
+            conn.Open();   
             varosokTablaLetrehozas();
-            idegenKulcsok();
+            latvanyosagTablaLetrehozas();
+
+
+
 
         }
         private void adatbazisLetrehiz()
@@ -34,23 +36,22 @@ namespace LatvanyossagokApplication
         void latvanyosagTablaLetrehozas()
         {
             var cmd = conn.CreateCommand();
-            cmd.CommandText = @"DROP TABLE IF EXISTS `latvanyossagok`;
-                                CREATE TABLE IF NOT EXISTS `latvanyossagok` (
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS `latvanyossagok` (
                                   `id` int(11) NOT NULL AUTO_INCREMENT,
                                   `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
                                   `leiras` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
                                   `ar` int(11) NOT NULL DEFAULT '0',
                                   `varos_id` int(11) NOT NULL,
                                   PRIMARY KEY (`id`),
-                                  KEY `varos_id` (`varos_id`)
+                                  KEY `varos_id` (`varos_id`),
+                                FOREIGN KEY(varos_id) REFERENCES varosok (id)
                                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
             cmd.ExecuteNonQuery();
         }
         void varosokTablaLetrehozas()
         {
             var cmd = conn.CreateCommand();
-            cmd.CommandText = @"DROP TABLE IF EXISTS `varosok`;
-                                CREATE TABLE IF NOT EXISTS `varosok` (
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS `varosok` (
                                   `id` int(11) NOT NULL AUTO_INCREMENT,
                                   `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
                                   `lakossag` int(11) NOT NULL,
@@ -59,13 +60,38 @@ namespace LatvanyossagokApplication
                                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
             cmd.ExecuteNonQuery();
         }
-        void idegenKulcsok()
+       
+        void VarosListazas()
+        {
+            listBoxVarosNevek.Items.Clear();
+            listBoxVarosok.Items.Clear();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT `id`, `nev`, `lakossag` FROM `varosok`";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32("id");
+                    var nev = reader.GetString("nev");
+                    var lakossag = reader.GetInt32("lakossag");
+                    listBoxVarosNevek.Items.Add(nev);
+                    listBoxVarosok.Items.Add(nev+"– Lakosság: "+lakossag+ " fő");
+                }
+            }
+        }
+        private void buttonVarosFeltolt_Click(object sender, EventArgs e)
         {
             var cmd = conn.CreateCommand();
-            cmd.CommandText = @"ALTER TABLE `latvanyossagok`
-                                ADD CONSTRAINT `latvanyossagok_ibfk_1` FOREIGN KEY(`varos_id`) REFERENCES `varosok` (`id`);";
-
+            cmd.CommandText = @"INSERT INTO varosok (nev, lakossag) VALUES (@nev, @lakossag)";
+            cmd.Parameters.AddWithValue("@nev", textBoxVarosNev.Text);
+            cmd.Parameters.AddWithValue("@lakossag", numericUpDownVarosLakossag.Value);
             cmd.ExecuteNonQuery();
+            VarosListazas();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VarosListazas();
         }
     }
 }
