df84dc6f03a3794001d06c5840dc7853b9507ece
TorokNiki <niki.torok@gmail.com>
2019-11-29 10:52:37 +0100

    Add project files.

---
+++ LatvanyossagokApplication/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatvanyossagokApplication
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
+            conn.Open();
+            latvanyosagTablaLetrehozas();
+            varosokTablaLetrehozas();
+            idegenKulcsok();
+
+        }
+        private void adatbazisLetrehiz()
+        {
+            /*
+            CREATE DATABASE IF NOT EXISTS `latvanyossagokdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+            USE `latvanyossagokdb`;*/
+        }
+        void latvanyosagTablaLetrehozas()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"DROP TABLE IF EXISTS `latvanyossagok`;
+                                CREATE TABLE IF NOT EXISTS `latvanyossagok` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `leiras` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `ar` int(11) NOT NULL DEFAULT '0',
+                                  `varos_id` int(11) NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  KEY `varos_id` (`varos_id`)
+                                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
+            cmd.ExecuteNonQuery();
+        }
+        void varosokTablaLetrehozas()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"DROP TABLE IF EXISTS `varosok`;
+                                CREATE TABLE IF NOT EXISTS `varosok` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `lakossag` int(11) NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE KEY `nev_indx` (`nev`)
+                                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
+            cmd.ExecuteNonQuery();
+        }
+        void idegenKulcsok()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"ALTER TABLE `latvanyossagok`
+                                ADD CONSTRAINT `latvanyossagok_ibfk_1` FOREIGN KEY(`varos_id`) REFERENCES `varosok` (`id`);";
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ LatvanyossagokApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatvanyossagokApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Hiba az adatbázis kapcsolodasakor!");
+                return;
+            }
+        }
+    }
+}
